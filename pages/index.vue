9d00dafa0eaee231dbd7be5ee6c7b4ffd8f0762a
Ivan Petrov <ivan.petrov@smarter-ecommerce.com>
2020-01-15 15:03:05 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        Welcome to my astonishing Nuxt.js project
+        Welcome to my astonishing Nuxt.js project!
       </h2>
       <div class="links">
         <a
